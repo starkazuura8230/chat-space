242e44d330ff88d965ee296d60b43a906ede16e0
starkazuura8230 <starkazu8230ura@gmail.com>
2020-01-23 11:48:19 +0900

    chat-route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'messages/index'
 
   root "messages#index"
-  resources :articles
-=======
 
-  root "messages#index"
->>>>>>> d52a8905cd78240a88fa49eaf9b92e4e485955b3
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
