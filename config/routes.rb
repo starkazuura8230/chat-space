66caa3e772483fe1a9375a63ff69c106cb2eb68c
starkazuura8230 <starkazu8230ura@gmail.com>
2020-01-23 21:39:56 +0900

    add-groudfunction-c

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
-
+  # resources :groups, only: [:new, :create]
+  resources :groups, only: [:index, :show, :new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
