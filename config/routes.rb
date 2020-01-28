e1db90c743b59d395b0195f6e99efc2636be151d
starkazuura8230 <starkazu8230ura@gmail.com>
2020-01-28 13:03:25 +0900

    add-message-send-comit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  # resources :groups, only: [:new, :create]
-  resources :groups, only: [:index, :show, :new, :create, :edit, :update]
+  resources :groups, only: [:show, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
