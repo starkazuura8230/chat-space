899ca46b44f4ef52f7318ec13a2342b84e81b83c
starkazuura8230 <starkazu8230ura@gmail.com>
2020-01-29 20:04:39 +0900

    hidouki-chat-3-comit3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,43 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="first" data-message-id=${message.id}>
+    if ( message.image ) {
+      var html =
+        `<div class="first" data-message-id=${message.id}>
+          <div class="upper-message">
+            <p class="upper-message__user-name">
+              ${message.user_name}
+            </p>
+            <p class="upper-message__date">
+              ${message.created_at}
+            </p>
+          </div>
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="first" data-message-id=${message.id}>
          <div class="upper-message">
-           <p class="upper-message__user-name">
-             ${message.user_name}
-           </p>
-           <p class="upper-message__date">
-             ${message.created_at}
-           </p>
-         </div>
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="first" data-message-id=${message.id}>
-         <div class="upper-message">
-           <p class="upper-message__user-name">
-             ${message.user_name}
-           </p>
-           <p class="upper-message__date">
-             ${message.created_at}
-           </p>
-         </div>
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+            <p class="upper-message__user-name">
+              ${message.user_name}
+            </p>
+            <p class="upper-message__date">
+              ${message.created_at}
+            </p>
+          </div>
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+});
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -52,12 +53,11 @@ $('#new_message').on('submit', function(e){
     .done(function(data){
       var html = buildHTML(data);
       $('.main-chat__message-list').append(html);      
-       $('#new_message')[0].reset();
-       $('.form__submit').prop('disabled', false);
-       $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.form__submit').prop('disabled', false);
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
     })
     .fail(function(data) {
       alert("メッセージ送信に失敗しました");
   });
 })
-});
