a4bbcb61e7db56f39a5317c3baa8ec87d842e893
starkazuura8230 <starkazu8230ura@gmail.com>
2020-02-04 17:18:14 +0900

    username

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '56.64.143.23', user: 'ec2-user', roles: %w{app db web}
+server '54.64.143.23', user: 'ec2-user', roles: %w{app db web}
